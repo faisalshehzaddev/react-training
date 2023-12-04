a7f5b8188764f9862795636fb2fd11172eeb773a
faisal shehzad <faisal.shehzad@devsinc.com>
2023-12-04 19:10:46 +0500

    functional code using use state

--- src/App.js
+++ src/App.js
@@ -1,15 +1,41 @@
 import logo from "./logo.svg";
 import "./App.css";
-import Counter from "./components/ClassComponents/Counter";
+import ClassCounter from "./components/ClassComponents/Counter";
+import FunctionalCounter from "./components/FunctionalComponents/Counter";
 import { useState } from "react";
 
 function App() {
-  const [showCounter, setShowCounter] = useState(false);
+  const [showClassCounter, setShowClassCounter] = useState(false);
+  const [showFunctionalCounter, setFunctionalCounter] = useState(false);
   return (
     <div className="App">
       <header className="App-header">
-        <button type="button" onClick={() => {setShowCounter(!showCounter)}}>Hide/Unhide</button>
-        {showCounter && <Counter />}</header>
+        <div>
+          <h1>Class Component</h1>
+          <button
+            type="button"
+            onClick={() => {
+              setShowClassCounter(!showClassCounter);
+            }}
+          >
+            Hide/Unhide
+          </button>
+          {showClassCounter && <ClassCounter />}
+        </div>
+        <br />
+        <div>
+          <h1>Functional Component</h1>
+          <button
+            type="button"
+            onClick={() => {
+              setFunctionalCounter(!showFunctionalCounter);
+            }}
+          >
+            Hide/Unhide
+          </button>
+          {showFunctionalCounter && <FunctionalCounter initialValue={0} />}
+        </div>
+      </header>
     </div>
   );
 }
